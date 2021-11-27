cbddb91a0aaea208dfb4e7f149839518886956f1
GEAR-2012 <sandor.tudja@gmail.com>
2021-11-27 11:22:26 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  AppBar,
+  Container,
+  createTheme,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  const navigate = useNavigate();
+
+  const { currency, setCurrency } = CryptoState();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography onClick={() => navigate("/")} className={classes.title} variant="h6">
+              Crypto Hunter
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+              value={currency}
+              onChange={(e) => {
+                setCurrency(e.target.value);
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"GBP"}>GBP</MenuItem>
+              <MenuItem value={"HUF"}>HUF</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
