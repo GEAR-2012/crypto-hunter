2652536ef017f38558ed4b6846fefb7d2f0adbec
GEAR-2012 <sandor.tudja@gmail.com>
2021-12-01 18:24:56 +0000

    remove console.logs

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -73,8 +73,6 @@ const UserSidebar = () => {
 
   const { user, setAlert, watchlist, coins, symbol } = CryptoState();
 
-  console.log(coins, watchlist);
-
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
       return;
